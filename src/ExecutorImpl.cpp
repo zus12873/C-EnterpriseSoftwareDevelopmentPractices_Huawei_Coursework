72614b0e4cde884aa4178e2dfc079f18bed9515e
zus12873 <cxnw020325@outlook.com>
2024-12-19 11:20:05 +0800

    test: Pass move command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -8,8 +8,30 @@ namespace adas
     ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : pose(pose)
     {
     }
+    // void ExecutorImpl::Execute(const std::string& commands) noexcept
+    // {
+    // }
     void ExecutorImpl::Execute(const std::string& commands) noexcept
     {
+            for (const auto cmd : commands)
+        {
+            if (cmd == 'M')
+            {
+                if (pose.heading == 'E')
+                {
+                    ++pose.x;
+                } else if (pose.heading == 'W') {
+                    --pose.x;
+                    
+                } else if (pose.heading == 'N') {
+                    ++pose.y;
+                    
+                } else if (pose.heading == 'S') {
+                    --pose.y;
+                    
+                }
+            }
+        }
     }
     Pose ExecutorImpl::Query() const noexcept
     {
