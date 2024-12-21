0d3f223f62f977065261ffc91baeca52f104f8e8
zus12873 <cxnw020325@outlook.com>
2024-12-21 13:02:24 +0800

    y PPT2 P10 task finished

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -1,6 +1,7 @@
 #include "ExecutorImpl.hpp" 
 namespace adas
 {
+    bool isFast = false;
     Executor* Executor::NewExecutor(const Pose& pose) noexcept
     {
             return new (std::nothrow) ExecutorImpl(pose);
@@ -12,31 +13,122 @@ namespace adas
     // {
     // }
     void ExecutorImpl::Execute(const std::string& commands) noexcept
+
     {
             for (const auto cmd : commands)
         {
+            if (cmd == 'F')
+            {
+                isFast = !isFast;
+            }
             if (cmd == 'M')
             {
+                if (isFast) {
+                    if (pose.heading == 'E')
+                    {
+                        ++pose.x;
+                                      
+                    }
+                    else if (pose.heading == 'W')
+                    {
+                        --pose.x;
+                                      
+                    }
+                    else if (pose.heading == 'N')
+                    {
+                        ++pose.y;                 
+                    }
+                    else if (pose.heading == 'S')
+                    {
+                        --pose.y;                  
+                    }      
+                }
+
                 if (pose.heading == 'E')
                 {
                     ++pose.x;
-                } else if (pose.heading == 'W') {
-                    --pose.x;
-                    
-                } else if (pose.heading == 'N') {
-                    ++pose.y;
-                    
-                } else if (pose.heading == 'S') {
-                    --pose.y;
-                    
+                                
+                }
+                else if (pose.heading == 'W')
+                {
+                    --pose.x;             
+                }
+                else if (pose.heading == 'N')
+                {
+                    ++pose.y;          
+                }
+                else if (pose.heading == 'S')
+                {
+                    --pose.y;               
+                }
+
+            } else if (cmd == 'L') {
+                if (isFast) {
+                    if (pose.heading == 'E') {
+                        ++pose.x;
+
+                    } else if (pose.heading == 'W') {
+                        --pose.x;
+
+                    } else if (pose.heading == 'N') {
+                        ++pose.y;
+                    } else if (pose.heading == 'S') {
+                        --pose.y;
+                    }
+                }
+                if (pose.heading == 'E')
+                {
+                    pose.heading = 'N';                
+                }
+                else if (pose.heading == 'N')
+                {
+                    pose.heading = 'W';      
+                }
+                else if (pose.heading == 'W')
+                {
+                    pose.heading = 'S';     
+                }
+                else if (pose.heading == 'S')
+                {
+                    pose.heading = 'E';    
+                }
+
+            } else if (cmd == 'R') {
+                if (isFast) {
+                    if (pose.heading == 'E') {
+                        ++pose.x;
+
+                    } else if (pose.heading == 'W') {
+                        --pose.x;
+
+                    } else if (pose.heading == 'N') {
+                        ++pose.y;
+                    } else if (pose.heading == 'S') {
+                        --pose.y;
+                    }
+                }
+                if (pose.heading == 'E') {
+                    pose.heading = 'S';                  
+                }
+                else if (pose.heading == 'S')
+                {
+                    pose.heading = 'W';                 
+                }
+                else if (pose.heading == 'W')
+                {
+                    pose.heading = 'N';         
+                }
+                else if (pose.heading == 'N')
+                {
+                    pose.heading = 'E';              
                 }
             }
-        }
-    }
-    Pose ExecutorImpl::Query() const noexcept
-    {
-            return pose;
+            }
     }
 
+                Pose ExecutorImpl::Query() const noexcept
+                {
+                    return pose;
+                }
 
-}// namespace adas
+            }  // namespace adas
--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -69,5 +69,88 @@ namespace adas
     const Pose target({0, -1, 'S'});
             ASSERT_EQ(target, executor->Query());
     }
+    TEST(ExecutorTest, should_return_facing_N_given_command_is_L_and_facing_is_E)
+    {
+             // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+             // when
+    executor->Execute("L");
+             // then
+    const Pose target({0, 0, 'N'});
+            ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_W_given_command_is_L_and_facing_is_N)
+    {
+             // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+             // when
+    executor->Execute("L");
+             // then
+    const Pose target({0, 0, 'W'});
+            ASSERT_EQ(target, executor->Query());
+    }
+
+    TEST(ExecutorTest, should_return_facing_S_given_command_is_L_and_facing_is_W)
+    {
+             // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+             // when
+    executor->Execute("L");
+             // then
+    const Pose target({0, 0, 'S'});
+            ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_E_given_command_is_L_and_facing_is_S)
+    {
+             // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+             // when
+    executor->Execute("L");
+             // then
+    const Pose target({0, 0, 'E'});
+            ASSERT_EQ(target, executor->Query());
+    }
+    // R
+    TEST(ExecutorTest, should_return_facing_S_given_command_is_R_and_facing_is_E)
+    {
+             // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+             // when
+    executor->Execute("R");
+             // then
+    const Pose target({0, 0, 'S'});
+            ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_W_given_command_is_R_and_facing_is_S)
+    {
+             // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+             // when
+    executor->Execute("R");
+             // then
+    const Pose target({0, 0, 'W'});
+            ASSERT_EQ(target, executor->Query());
+    }
+
+    TEST(ExecutorTest, should_return_facing_N_given_command_is_R_and_facing_is_W)
+    {
+             // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+             // when
+    executor->Execute("R");
+             // then
+    const Pose target({0, 0, 'N'});
+            ASSERT_EQ(target, executor->Query());
+    }
+    TEST(ExecutorTest, should_return_facing_E_given_command_is_R_and_facing_is_N)
+    {
+             // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+             // when
+    executor->Execute("R");
+             // then
+    const Pose target({0, 0, 'E'});
+            ASSERT_EQ(target, executor->Query());
+    }
 
 } // namespace adas
