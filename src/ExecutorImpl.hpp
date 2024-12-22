f36ccfa6a4f7ae7c2a6b36f32e49f5bd704d9054
Meng Huan <menghuan@MengdeMac-mini.local>
2024-12-22 10:34:41 +0800

    y PPT3 P24,5,6 finished

--- src/ExecutorImpl.hpp
+++ src/ExecutorImpl.hpp
@@ -17,4 +17,5 @@ private:
 
        
 };
+
 }  // namespace adas 
--- src/PoseHandler.hpp
+++ src/PoseHandler.hpp
@@ -21,4 +21,20 @@ namespace adas
        Pose pose;
        bool fast{false};
     };
+    class Point final
+    {
+    public:
+        Point(const int x, const int y) noexcept;
+        Point(const Point& rhs) noexcept;
+        Point& operator=(const Point& rhs) noexcept;
+        Point& operator+=(const Point& rhs) noexcept;
+
+    public:
+        int GetX(void) const noexcept;
+        int GetY(void) const noexcept;
+
+    private:
+        int x;
+        int y;
+    };
 }  // namespace adas
